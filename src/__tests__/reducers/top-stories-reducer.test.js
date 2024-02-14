c6ff41a6a4a0facb7058f83332b55506158c016c
jessicahattig <jessicahattig@gmail.com>
2024-02-13 20:33:23 -0800

    Red and green phase top-stories-reducer.test.js second test.

--- src/__tests__/reducers/top-stories-reducer.test.js
+++ src/__tests__/reducers/top-stories-reducer.test.js
@@ -1,6 +1,9 @@
 import topStoriesReducer from '../../reducers/top-stories-reducer';
+import * as c from './../../actions/ActionTypes';
 
 describe('topStoriesReducer', () => {
+
+  let action;
 
   const initialState = {
     isLoaded: false,
@@ -14,5 +17,19 @@ describe('topStoriesReducer', () => {
         topStoriesReducer(initialState, {type: null })
       }
     ).toThrowError("There is no action matching null.");
+  });
+
+  test('successfully getting top stories should change isLoaded to true and update topStories', () => {
+    const topStories = "An article";
+    action = {
+      type: c.GET_TOP_STORIES_SUCCESS,
+      topStories
+    };
+
+    expect(topStoriesReducer(initialState, action)).toEqual({
+        isLoaded: true,
+        topStories: "An article",
+        error: null
+    });
   });
 });
